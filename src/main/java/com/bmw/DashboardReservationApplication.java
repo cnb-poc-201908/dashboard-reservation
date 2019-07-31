667c95c3490999d4510021a44701b5ccc6f1a546
LIANG XIONG <xionglwh@cn.ibm.com>
2019-07-31 21:29:58 +0800

    Change the base package name of application.

--- src/main/java/com/bmw/DashboardReservationApplication.java
+++ src/main/java/com/bmw/DashboardReservationApplication.java
@@ -1,4 +1,4 @@
-package com.ibm.bmwpoc;
+package com.bmw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/bmw/controller/HelloEndpoint.java
+++ src/main/java/com/bmw/controller/HelloEndpoint.java
@@ -1,4 +1,4 @@
-package com.ibm.bmwpoc.controller;
+package com.bmw.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
