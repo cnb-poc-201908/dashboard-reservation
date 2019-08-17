94d6ec7aa5e6331d786719fc0a802673740ab351
Jane1016 <zjunwh@cn.ibm.com>
2019-08-17 12:06:16 +0800

    update for adding search

--- src/main/java/com/bmw/controller/BasicInfoController.java
+++ src/main/java/com/bmw/controller/BasicInfoController.java
@@ -1,17 +1,18 @@
 package com.bmw.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
 import com.bmw.entity.response.RespForObject;
-import com.bmw.entity.response.ResponseForCustomerList;
 import com.bmw.service.BasicInfoService;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -20,8 +21,11 @@ import com.google.gson.JsonParser;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang.StringUtils;
+import java.lang.reflect.Field;
 
 @RestController
 @Api(description = "basic info Controller")
@@ -48,6 +52,46 @@ public class BasicInfoController {
 		response.setMessage("成功");
 		response.setBasicInfoList(basicList);
 		
+		return response;
+	}
+	
+	@RequestMapping(value = "/GetBasicInfo/{regid}", method = RequestMethod.GET, produces = "application/json")
+	@ApiOperation(value = "指定车辆查询")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "查询成功"), @ApiResponse(code = 400, message = "错误的请求"),
+			@ApiResponse(code = 401, message = "没有权限查看此资源"), @ApiResponse(code = 404, message = "资源不存在"),
+			@ApiResponse(code = 500, message = "内部错误请联系管理员") })
+	public RespForObject getBasicInfoByRegId(@ApiParam("regid") @PathVariable("regid") String regid) {
+		RespForObject response = new RespForObject();
+		Gson gson = new Gson();
+		JsonParser parser = new JsonParser();
+		JsonObject basicObject = (JsonObject) parser.parse(basicapp.getAllBasicInfoList());
+		JsonArray basicArray = basicObject.get("basic_info").getAsJsonArray();
+		List<Object> basicList = JSONObject.parseArray(basicArray.toString(), Object.class);
+		if(StringUtils.isNotBlank(regid) ) {
+			for(int i=0; i < basicArray.size(); i++) {
+				String objItem = basicArray.get(i).toString();
+				JSONObject basicObj = JSONObject.parseObject(objItem);
+				String vercleItem = basicObj.getString("virecle_info");
+				if(vercleItem.contains(regid)) {
+					response.setCode(200);
+					response.setMessage("成功");
+					List<Object> basicItemList = new ArrayList<>();
+					basicItemList.add(basicObj);
+					response.setBasicInfoList(basicItemList);
+					break;
+				}
+				else {
+					response.setCode(0);
+					response.setMessage("没有记录");
+				}
+			}
+			
+		}
+		else {
+			response.setCode(200);
+			response.setMessage("成功");
+			response.setBasicInfoList(basicList);
+		}
 		return response;
 	}
 	
